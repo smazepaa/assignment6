e4cd56027433625067a2a1f98782a8296485b16d
smazepaa <smazepa@kse.ord.ua>
2022-12-07 22:08:16 +0200

    worked with file, extracted variables

---
+++ main.py
@@ -0,0 +1,15 @@
+with open('olympic athletes.tsv', 'r') as file:
+    next_line = file.readline()
+    while next_line:
+        next_line = file.readline()
+        split_line = next_line.split('\t')
+        noc_line = split_line[7]
+        year_line = split_line[9]
+        medal_line = split_line[-1][:-1]
+        name_athlete = split_line[1]
+        sport_athlete = split_line[-3]
+        event_line = split_line[-2]
+
+
+
+
