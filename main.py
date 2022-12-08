24e5aa3405ad52ec676e6c42fe7ea2055d7154e2
smazepaa <smazepa@kse.ord.ua>
2022-12-08 12:00:29 +0200

    changed name of tsv to olympic_athletes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ command = sys.argv[1]
 country = sys.argv[2]
 year = sys.argv[-1]
 
-with open('olympic athletes.tsv', 'r') as file:
+with open('olympic_athletes.tsv', 'r') as file:
     next_line = file.readline()
     while next_line:
         next_line = file.readline()
@@ -14,4 +14,5 @@ with open('olympic athletes.tsv', 'r') as file:
         medal_line = split_line[-1][:-1]
         name_athlete = split_line[1]
         sport_athlete = split_line[-3]
-        event_line = split_line[-2]
+
+
