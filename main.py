68b485f8c040b3db047f5c6098b24c425e43a521
Asta Motrenko <amotrenko@kse.org.ua>
2022-12-09 11:09:55 +0200

    added def if_medal, rename filename

--- main.py
+++ main.py
@@ -16,47 +16,56 @@ def count_medals(medals_list, year_ol):
     print(f'in {year_ol}, the country won {gold} gold, '
           f'{silver} silver and {bronze} bronze medals')
 
-command = sys.argv[1]
-country = sys.argv[2]
+
+
+def if_medal(filename, country, year):
+    counter = 0
+    names = []
+    medals = []
+
+    with open(filename, 'r') as file:
+        file.readline()
+        next_line = file.readline()
+        while next_line:
+            split_line = next_line.split('\t')
+            noc_line = split_line[7]
+            year_line = split_line[9]
+            medal_line = split_line[-1][:-1]
+            name_athlete = split_line[1]
+            sport_athlete = split_line[-3]
+
+            if country in split_line:
+                if year in split_line:
+                    while counter < 10:
+                        if name_athlete not in names and medal_line != 'NA':
+                            print(f'{counter + 1}. {name_athlete} - {sport_athlete} - {medal_line}')
+                            counter += 1
+                            names.append(name_athlete)
+
+                        else:
+                            break
+
+                    medals.append(medal_line)
+
+            next_line = file.readline()
+
+        if len(names) == 0:
+            print('invalid input (either country or year)')
+            quit()
+
+        if counter < 10:
+            print(f'in {year} {country} had only {counter} medalists')
+
+        count_medals(medals, year)
+
+
+file_name = sys.argv[1]
+command = sys.argv[2]
+country = sys.argv[3]
 year = sys.argv[-1]
 
-counter = 0
-names = []
-medals = []
+if command == "-medal":
+    if_medal(file_name, country, year)
 
-with open('olympic_athletes.tsv', 'r') as file:
-    file.readline()
-    next_line = file.readline()
-    while next_line:
-        split_line = next_line.split('\t')
-        noc_line = split_line[7]
-        year_line = split_line[9]
-        medal_line = split_line[-1][:-1]
-        name_athlete = split_line[1]
-        sport_athlete = split_line[-3]
-
-        if country in split_line:
-            if year in split_line:
-                while counter < 10:
-                    if name_athlete not in names and medal_line != 'NA':
-                        print(f'{counter + 1}. {name_athlete} - {sport_athlete} - {medal_line}')
-                        counter += 1
-                        names.append(name_athlete)
-
-                    else:
-                        break
-
-                medals.append(medal_line)
-
-        next_line = file.readline()
-
-    if len(names) == 0:
-        print('invalid input (either country or year)')
-        quit()
-
-    if counter < 10:
-        print(f'in {year} {country} had only {counter} medalists')
-
-    count_medals(medals, year)
 
 
