dd4d5fb1d62e4e7c3175e48e7b3fae1fad6e8402
smazepaa <smazepa@kse.ord.ua>
2022-12-13 22:30:30 +0200

    added if_overall (task 3)

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def count_medals(medals_list, year_ol):
         elif medal == 'Bronze':
             bronze += 1
 
-    print(f'in {year_ol}, the country won {gold} gold, '
+    print(f'in {year_ol},country won {gold} gold, '
           f'{silver} silver and {bronze} bronze medals')
 
 
@@ -90,6 +90,34 @@ def if_total(filename, year):
             print(f'{key}: {value[0]} gold - {value[1]} silver - {value[2]} bronze')
 
 
+def if_overall(filename, country):
+    dict_countries = {}
+    with open(filename, 'r') as file:
+        file.readline()
+        next_line = file.readline()
+        while next_line:
+            split_line = next_line.split('\t')
+            medal_line = split_line[-1][:-1]
+            noc_line = split_line[7]
+            year_line = split_line[9]
+
+            if noc_line not in dict_countries:
+                dict_countries[noc_line] = {}
+            if year_line not in dict_countries[noc_line]:
+                dict_countries[noc_line][year_line] = 0
+            if medal_line == 'Gold' or medal_line == 'Silver' or medal_line == 'Bronze':
+               dict_countries[noc_line][year_line] += 1
+
+            next_line = file.readline()
+
+    max_value = max(dict_countries[country].values())
+    max_key = max(dict_countries[country], key=dict_countries[country].get)
+    min_value = min(dict_countries[country].values())
+    min_key = min(dict_countries[country], key=dict_countries[country].get)
+
+    return max_value, max_key, min_value, min_key
+
+
 if len(args.country) > 3:
     args.country = pycountry.countries.get(name=args.country).alpha_3
 
@@ -97,6 +125,11 @@ if args.medals:
     if_medal(args.filename, args.country, args.year)
 elif args.total:
     if_total(args.filename, args.year)
+elif args.overall:
+    maximum = if_overall("olympic_athletes.tsv", args.country)
+    max_meds = maximum[0]
+    year_max = maximum[1]
+    print(f'{args.country}: the best year was {year_max} - country won {max_meds} medals')
 
 
 
