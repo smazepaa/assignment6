6751f8f5ab5ee525dd68b9fb5d508b93eb6e3d52
Asta Motrenko <amotrenko@kse.org.ua>
2022-12-13 21:20:14 +0200

    added if_total (task2)

--- main.py
+++ main.py
@@ -50,6 +50,35 @@ def if_medal(filename, country, year):
         count_medals(medals, year)
 
 
+def if_total(filename, year):
+    dict_countries = {}
+    with open(filename, 'r') as file:
+        file.readline()
+        next_line = file.readline()
+        while next_line:
+            split_line = next_line.split('\t')
+            medal_line = split_line[-1][:-1]
+            noc_line = split_line[7]
+
+            if year in split_line and medal_line != 'NA':
+                if noc_line not in dict_countries:
+                    dict_countries[noc_line] = [0, 0, 0]  # gold-silver-bronze
+
+                if medal_line == 'Gold':
+                    dict_countries[noc_line][0] += 1
+                elif noc_line in dict_countries and medal_line == 'Silver':
+                    dict_countries[noc_line][1] += 1
+                elif noc_line in dict_countries and medal_line == 'Bronze':
+                    dict_countries[noc_line][2] += 1
+
+            next_line = file.readline()
+    if len(dict_countries) == 0:
+        print('there was no olympiad that year')
+    else:
+        for key, value in dict_countries.items():  # iterating items
+            print(f'{key}: {value[0]} gold - {value[1]} silver - {value[2]} bronze')
+
+
 file_name = sys.argv[1]
 command = sys.argv[2]
 country_c = sys.argv[3]
